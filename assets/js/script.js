a18a0221e1d6d1a008967901516dbd4d2f102e51
novalie11 <novamarialie11@gmail.com>
2024-03-11 17:07:40 +0100

    new
    design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
-// if you use jQuery, you need to start your .js file with this.
-// otherwise you can delete everything here.
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-// functions go here
+  function scrollFunction() {
+    var mybutton = document.getElementById("mybutton");
 
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
+
+  // Attach click event listener to the button
+  var mybutton = document.getElementById("mybutton");
+  mybutton.addEventListener("click", topFunction);
+
+  const links = document.querySelectorAll(".visual-event-link");
+  const images = document.querySelectorAll(".visual-event-link");
 });
